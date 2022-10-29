992bbed71acf33c8f4a5096e1fc6217c12ad699b
kingsloc <kingsleynwankwou@gmail.com>
2022-10-29 13:17:48 +0100

    app

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func bio(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(map[string]interface{}{
+			"status":  http.StatusOK,
+			"success": true,
+			"data": map[string]interface{}{
+				"slackUsername": "Kingsley Nwankwo",
+				"backend":       true,
+				"age":           28,
+				"bio":           "I am Kingsley and I build backend stuff using golang.",
+			},
+		})
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error building the response, %v", err), http.StatusInternalServerError)
+			return
+		}
+	default:
+		http.Error(w, fmt.Sprintf("method %s is not allowed", r.Method), http.StatusMethodNotAllowed)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", bio)
+
+	err := http.ListenAndServe(":3333", mux)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("Server closed\n")
+	} else if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
