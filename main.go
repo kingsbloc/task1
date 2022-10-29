6495e000a3a92850e10cda400ba388a1cdefcde5
kingsloc <kingsleynwankwou@gmail.com>
2022-10-29 23:02:15 +0100

    change api response

--- main.go
+++ main.go
@@ -14,14 +14,10 @@ func bio(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		w.Header().Set("Content-Type", "application/json")
 		err := json.NewEncoder(w).Encode(map[string]interface{}{
-			"status":  http.StatusOK,
-			"success": true,
-			"data": map[string]interface{}{
-				"slackUsername": "Kingsley Nwankwo",
-				"backend":       true,
-				"age":           28,
-				"bio":           "I am Kingsley and I build backend stuff using golang.",
-			},
+			"slackUsername": "Kingsley Nwankwo",
+			"backend":       true,
+			"age":           28,
+			"bio":           "I am Kingsley and I build backend stuff using golang.",
 		})
 		if err != nil {
 			http.Error(w, fmt.Sprintf("error building the response, %v", err), http.StatusInternalServerError)
